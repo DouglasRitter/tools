bb0f815c991365f42db7d585ec5b92219c85b009
DouglasRitter <79718584+DouglasRitter@users.noreply.github.com>
2023-03-31 08:32:13 -0500

    Create rotate_aws_credentials.py

---
+++ aws/rotate_aws_credentials.py
@@ -0,0 +1,32 @@
+import boto3
+
+def rotate_aws_credentials(user_name, current_access_key, current_secret_key):
+    session = boto3.Session(
+        aws_access_key_id=current_access_key,
+        aws_secret_access_key=current_secret_key
+    )
+    iam = session.client('iam')
+
+    # Create a new access key
+    response = iam.create_access_key(UserName=user_name)
+    new_access_key = response['AccessKey']
+
+    # Print the new access key
+    print("New access key:")
+    print(f"Access key ID: {new_access_key['AccessKeyId']}")
+    print(f"Secret access key: {new_access_key['SecretAccessKey']}")
+
+    # Set the input key as inactive
+    iam.update_access_key(
+        UserName=user_name,
+        AccessKeyId=current_access_key,
+        Status='Inactive'
+    )
+    print("The input access key has been set as inactive.")
+
+if __name__ == '__main__':
+    user_name = input("Enter the AWS user name: ")
+    current_access_key = input("Enter the current access key ID: ")
+    current_secret_key = input("Enter the current secret access key: ")
+
+    rotate_aws_credentials(user_name, current_access_key, current_secret_key)
